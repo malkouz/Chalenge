dffcdcfb278f9cfb34376bb154fb81f4d3c8fa60
Moayad <moayadalkouz@Moayads-MacBook-Pro.local>
2017-04-22 11:32:02 +0300

    first commit

---
+++ Chalenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chalenge
+//
+//  Created by Moayad Al kouz on 7/25/1438 AH.
+//  Copyright © 1438 Moayad Al kouz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
